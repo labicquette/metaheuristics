9e404bfb1ede874a7467e44ea9edf3f98a665b56
labicquette <komete17@gmail.com>
2023-10-10 21:45:22 +0200

    added working?

--- eval.jl
+++ eval.jl
@@ -1,10 +1,11 @@
 function eval(dir, nbRuns)
     files = getfname(dir)
-    C, A = loadSPP(string(dir,files[1]))
+    C, A = loadSPP(string("./Data/didactic.dat"))
     s = sum(A, dims=1)
     C = reshape(C,  1, length(C))
     v = C ./ s
     sorted = sortperm(v, dims = 2, rev=true)
+    x0, res = optimiz(C, A)
     x0, res = optimiz(C, A)
     time_runs = zeros(length(files))
     for i in 1:length(files)
@@ -17,11 +18,12 @@ function eval(dir, nbRuns)
         for j in 1:nbRuns
             start = time()
             x0, xbest, res = optimiz(C, A)
+            println("x0 : ",x0, ", xbest: ",xbest)
             temp_time += (time() - start)
         end
         time_runs[i] = temp_time / nbRuns
         #println(time_runs[i])
         #W = transpose(res) .* C
     end
-    #println(time_runs)
+    println(time_runs)
 end 
--- mainspp.jl
+++ mainspp.jl
@@ -1,22 +1,12 @@
 
+
 using JuMP, GLPK
 using LinearAlgebra
 
 include("loadSPP.jl")
 include("setSPP.jl")
 include("getfname.jl")
+include("eval.jl")
+include("solvers.jl")
 
-
-println("\nLoading...")
-fname = "Data/didactic.dat"
-C, A = loadSPP(fname)
-@show C
-@show A
-
-s = sum(A, dims=1)
-C = reshape(C,  1, length(C))
-println("C : ", C)
-println("S : ", s)
-v = C ./ s
-sorted = sort(v, dims = 2, rev=true)
-println("sort", sorted)
+eval("./bigdata/", 5)
